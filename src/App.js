b4261ab64980f0b14453053e21658fa8f45c7d00
Najji09 <n96h09j09@outlook.kr>
2022-03-18 14:00:53 +0900

    v0.1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Component_Props from './Component_Props';
+import Clock from './State_life_Cycle';
+function App() {
+  return (
+    <div className="App">
+      <Component_Props />
+      <Clock />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component_Props.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+function Avartar(props) {
+  return (
+    <img className="Avatar" src={props.user.avatar} alt={props.user.name} />
+  );
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avartar user={props.user} />
+      <div className="UserInfo-name">{props.user.name}</div>
+    </div>
+  );
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-txt">{props.txt}</div>
+      <div className="Comment-date">{formatDate(props.date)}</div>
+    </div>
+  );
+}
+
+const commentOne = {
+  date: new Date(),
+  txt: 'Nice to meet you!',
+  author: {
+    name: 'cat1',
+    avatar: 'http://placekitten.com/g/64/64',
+  },
+};
+
+const commentTwo = {
+  date: new Date(),
+  txt: 'Nice to meet you Too!',
+  author: {
+    name: 'cat2',
+    avatar: 'http://placekitten.com/g/64/64',
+  },
+};
+
+let Component_Props = () => {
+  return (
+    <div className="component_props">
+      <Comment
+        date={commentOne.date}
+        txt={commentOne.txt}
+        author={commentOne.author}
+      />
+      <Comment
+        date={commentTwo.date}
+        txt={commentTwo.txt}
+        author={commentTwo.author}
+      />
+    </div>
+  );
+};
+
+export default Component_Props;
